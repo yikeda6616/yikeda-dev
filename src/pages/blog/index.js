d8cc3a3df91b5b90fbaff43293caf1ed07341a33
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-13 16:32:42 -0700

    Update heading style

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -15,14 +15,14 @@ export default class BlogIndexPage extends React.Component {
         >
           <h1
             className='has-text-weight-bold is-size-1'
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem'
-            }}
+            // style={{
+            //   boxShadow: '0.5rem 0 0 #171717, -0.5rem 0 0 #171717',
+            //   backgroundColor: '#171717',
+            //   color: 'white',
+            //   padding: '1rem'
+            // }}
           >
-            Latest Stories
+            Latest Posts
           </h1>
         </div>
         <section className='section'>
