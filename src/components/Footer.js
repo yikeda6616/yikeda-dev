ea9a447b1cb34ba09f0293d1bb5928b5b6ad52a2
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-13 18:23:08 -0700

    Add social icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
 import logo from '../img/logo.svg';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
 import twitter from '../img/social/twitter.svg';
-import vimeo from '../img/social/vimeo.svg';
+import linkedin from '../img/social/linkedin.svg';
 
 const Footer = class extends React.Component {
   render() {
@@ -74,35 +75,42 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className='column is-4 social'>
-                <a title='facebook' href='https://facebook.com'>
+                <OutboundLink href='https://www.linkedin.com/in/yasushi-ikeda-0aa72b153/'>
+                  <img
+                    src={linkedin}
+                    alt='LinkedIn'
+                    style={{ width: '1rem', height: '1rem' }}
+                  />
+                </OutboundLink>
+                <OutboundLink href='https://www.facebook.com/yasushi.ikeda.3910'>
                   <img
                     src={facebook}
                     alt='Facebook'
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
-                <a title='twitter' href='https://twitter.com'>
+                </OutboundLink>
+                <OutboundLink href='https://twitter.com/yikeda6616'>
                   <img
                     className='fas fa-lg'
                     src={twitter}
                     alt='Twitter'
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
-                <a title='instagram' href='https://instagram.com'>
+                </OutboundLink>
+                <OutboundLink href='https://instagram.com/yikeda6616'>
                   <img
                     src={instagram}
                     alt='Instagram'
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
-                <a title='vimeo' href='https://vimeo.com'>
+                </OutboundLink>
+                {/* <a title='vimeo' href='https://vimeo.com'>
                   <img
                     src={vimeo}
                     alt='Vimeo'
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
