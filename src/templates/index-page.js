bcce408888c12daf3830a4f0911927f7ac168440
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-13 17:26:42 -0700

    Update index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -95,7 +95,7 @@ export const IndexPageTemplate = ({
                 </div> */}
                 <div className='column is-12'>
                   <h3 className='has-text-weight-semibold is-size-2'>
-                    Latest stories
+                    Latest posts
                   </h3>
                   <BlogRoll />
                   <div className='column is-12 has-text-centered'>
